2fda552313e33db7f08b13678bfc141e2736fb16
Mike Decker <pookmish@gmail.com>
2019-08-27 18:34:18 -0700

    finished sound cloud provider test

--- tests/src/Unit/Plugin/audio_embed_field/Provider/StanfordSoundCloudTest.php
+++ tests/src/Unit/Plugin/audio_embed_field/Provider/StanfordSoundCloudTest.php
@@ -11,6 +11,12 @@ use GuzzleHttp\Exception\ClientException;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
+/**
+ * Class StanfordSoundCloudTest.
+ *
+ * @group stanford_media
+ * @coversDefaultClass \Drupal\stanford_media\Plugin\audio_embed_field\Provider\StanfordSoundCloud
+ */
 class StanfordSoundCloudTest extends UnitTestCase {
 
   /**
@@ -18,10 +24,23 @@ class StanfordSoundCloudTest extends UnitTestCase {
    */
   protected $container;
 
+  /**
+   * If the mock guzzle callback should return a response.
+   *
+   * @var bool
+   */
   protected $guzzleResponse = TRUE;
 
+  /**
+   * If the mock cache service should return a cached value.
+   *
+   * @var bool
+   */
   protected $returnCachedResponse = FALSE;
 
+  /**
+   * {@inheritDoc}
+   */
   protected function setUp() {
     parent::setUp();
     $guzzle = $this->createMock(ClientInterface::class);
@@ -61,13 +80,42 @@ class StanfordSoundCloudTest extends UnitTestCase {
     $this->assertInstanceOf(StanfordSoundCloud::class, $plugin);
   }
 
+  /**
+   * Test the simple methods.
+   */
+  public function testOtherMethods() {
+    $plugin = StanfordSoundCloud::create($this->container, ['input' => $this->randomMachineName()], '', []);
+    $this->assertEquals('http://foo.bar/image.jpg', $plugin->getRemoteThumbnailUrl());
+    $this->assertEquals('Foo Bar', $plugin->getName());
+
+    $this->guzzleResponse = FALSE;
+    $this->assertNull($plugin->getRemoteThumbnailUrl());
+  }
+
+  /**
+   * Test the render array is structured correctly.
+   */
+  public function testRenderEmbedCode() {
+    $plugin = StanfordSoundCloud::create($this->container, ['input' => $this->randomMachineName()], '', []);
+    $render_array = $plugin->renderEmbedCode(100, 100, FALSE);
+
+    $this->assertEquals('audio_embed_iframe', $render_array['#type']);
+    $this->assertEquals('https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/740178957', $render_array['#url']);
+    $this->assertArraySubset([
+      'width' => 100,
+      'height' => 100,
+      'frameborder' => 0,
+    ], $render_array['#attributes']);
+  }
+
+  /**
+   * Mock guzzle client response callback.
+   */
   public function guzzleRequestCallback() {
     $response = $this->createMock(ResponseInterface::class);
     if ($this->guzzleResponse === TRUE) {
-      $body = [
-        'html' => '<iframe src="https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F647171277&show_artwork=true\"></iframe>',
-      ];
-      $response->method('getBody')->willReturn(json_encode($body));
+      $response->method('getBody')
+        ->willReturn(json_encode($this->getVideoData()));
     }
     if (is_null($this->guzzleResponse)) {
       $request = $this->createMock(RequestInterface::class);
@@ -75,20 +123,31 @@ class StanfordSoundCloudTest extends UnitTestCase {
       throw new ClientException('It broke.', $request, $response);
     }
     if ($this->guzzleResponse === FALSE) {
-      $body = [];
-      $response->method('getBody')->willReturn(json_encode($body));
+      $response->method('getBody')->willReturn(json_encode([]));
     }
     return $response;
   }
 
+  /**
+   * Mock cache service callback.
+   */
   public function getCacheCallback() {
     if ($this->returnCachedResponse) {
       $cache_item = new \stdClass();
-      $cache_item->data = [
-        'html' => '<iframe src="https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F647171277&show_artwork=true\"></iframe>',
-      ];
+      $cache_item->data = $this->getVideoData();
       return $cache_item;
     }
+  }
+
+  /**
+   * Get the SoundCloud api response data.
+   */
+  protected function getVideoData() {
+    return [
+      'html' => '<iframe src="https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Fplaylists%2F740178957&show_artwork=true"></iframe>',
+      'thumbnail_url' => 'http://foo.bar/image.jpg',
+      'title' => 'Foo Bar',
+    ];
   }
 
 }
