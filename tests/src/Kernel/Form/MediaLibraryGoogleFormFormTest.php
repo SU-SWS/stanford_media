2ec9d53d4f865aaeef2f700065bbae318a59bbe3
Mike Decker <pookmish@gmail.com>
2020-04-02 16:35:37 -0700

    more coverage

--- tests/src/Kernel/Form/MediaLibraryGoogleFormFormTest.php
+++ tests/src/Kernel/Form/MediaLibraryGoogleFormFormTest.php
@@ -2,9 +2,11 @@
 
 namespace Drupal\Tests\stanford_media\Kernel\Form;
 
+use Drupal\Core\Form\FormState;
 use Drupal\media\Entity\Media;
 use Drupal\media\Entity\MediaType;
 use Drupal\media_library\MediaLibraryState;
+use Drupal\stanford_media\Form\MediaLibraryGoogleFormForm;
 use Drupal\Tests\user\Traits\UserCreationTrait;
 
 /**
@@ -36,10 +38,13 @@ class MediaLibraryGoogleFormFormTest extends StanfordMediaFormTestBase {
     $source_field->save();
     $media_type->set('source_configuration', ['source_field' => $source_field->getName()])->save();
 
-    $user = $this->createUser(['create google_form media']);
+    $user = $this->createUser(['create google_form media', 'view media']);
     $this->setCurrentUser($user);
   }
 
+  /**
+   * Media library add form should have the url input.
+   */
   public function testMediaLibraryForm() {
     $params = [
       'media_library_opener_id' => 'foo-bar',
@@ -49,12 +54,41 @@ class MediaLibraryGoogleFormFormTest extends StanfordMediaFormTestBase {
       'media_library_content' => '1',
     ];
     $state = new MediaLibraryState($params);
-    /** @var \Drupal\media_library\MediaLibraryUiBuilder $ui_builder */
-    $ui_builder = \Drupal::service('media_library.ui_builder');
-    $ui = $ui_builder->buildUi($state);
 
-    $this->assertArrayHasKey('url', $ui['form']['container']);
-    $this->assertArrayHasKey('submit', $ui['form']['container']);
+    $form_state = new FormState();
+    $form_state->set('media_library_state', $state);
+    $form = \Drupal::formBuilder()->buildForm(MediaLibraryGoogleFormForm::class, $form_state);
+
+    $this->assertArrayHasKey('url', $form['container']);
+    $this->assertArrayHasKey('submit', $form['container']);
+
+    /** @var \Drupal\stanford_media\Form\MediaLibraryGoogleFormForm $form_object */
+    $form_object = $form_state->getFormObject();
+    $form_object->buildForm($form, $form_state);
+
+    $this->assertEmpty($form_state->get('current_selection'));
+    $form_state->setValue('url', 'http://google.com');
+    $form_object->addButtonSubmit($form, $form_state);
+    $this->assertCount(1, $form_state->get('media'));
+  }
+
+  /**
+   * Building the form for the incorrect media type should throw an error.
+   */
+  public function testFormException() {
+    $params = [
+      'media_library_opener_id' => 'foo-bar',
+      'media_library_allowed_types' => ['file'],
+      'media_library_selected_type' => 'file',
+      'media_library_remaining' => 1,
+      'media_library_content' => '1',
+    ];
+    $state = new MediaLibraryState($params);
+
+    $form_state = new FormState();
+    $form_state->set('media_library_state', $state);
+    $this->expectException('\InvalidArgumentException');
+    \Drupal::formBuilder()->buildForm(MediaLibraryGoogleFormForm::class, $form_state);
   }
 
 }
