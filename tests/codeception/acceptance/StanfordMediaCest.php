86a2fa2e4c739fe28d9639ce0b92e444983e22f3
pookmish <pookmish@stanford.edu>
2021-02-08 10:53:37 -0800

    D8CORE-3425 Patch drupal core for better embeddable handling (#88)

--- tests/codeception/acceptance/StanfordMediaCest.php
+++ tests/codeception/acceptance/StanfordMediaCest.php
@@ -2,9 +2,16 @@
 
 class StanfordMediaCest {
 
-  public function testSomething(AcceptanceTester $I){
-    $I->amOnPage('/');
-    $I->canSeeResponseCodeIs(200);
+  /**
+   * ArcGis Embeddables can be saved.
+   */
+  public function testArcGis(AcceptanceTester $I){
+    $I->logInWithRole('administrator');
+    $I->amOnPage('/media/add/embeddable');
+    $I->fillField('Name','ArcGis');
+    $I->fillField('oEmbed URL', 'https://storymaps.arcgis.com/stories/4586c60dc91744cbae9967442f990468');
+    $I->click('Save');
+    $I->canSee('Embeddable ArcGis has been created.');
   }
 
 }
