7577455a23973f3c144832ece2cfd074467bb2fa
Mike Decker <pookmish@stanford.edu>
2023-12-19 08:32:47 -0800

    Deprecated filter plugin

--- src/Plugin/Filter/MediaEmbedMarkupFilter.php
+++ src/Plugin/Filter/MediaEmbedMarkupFilter.php
@@ -8,10 +8,14 @@ use Drupal\filter\FilterProcessResult;
 /**
  * MediaEmbedMarkupFilter for fixing core issues.
  *
+ * @deprecated in 10.1.5 and removed in 11.0.0.
+ *   Fixed in https://www.drupal.org/project/drupal/issues/1333730.
+ *   No longer necessary since Drupal 10.2.0 WYSIWYG supports HTML5.
+ *
  * @Filter(
  *   id = "stanford_media_embed_markup",
  *   title = @Translation("Stanford Media Embed Filter"),
- *   description = @Translation("This helps with core markup issues. This filter has to run after the Embed Media filter."),
+ *   description = @Translation("DEPRECATED: No longer necessary in Drupal 10.2.0+. This helps with core markup issues. This filter has to run after the Embed Media filter."),
  *   type = Drupal\filter\Plugin\FilterInterface::TYPE_TRANSFORM_IRREVERSIBLE,
  * )
  */
